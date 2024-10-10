e35545b7866b8340a5cd2425cb2eb944a50096eb
Jake Chapman <jake@imjakechapman.com>
2024-10-10 14:56:45 -0700

    Snazzy Swizzle.

--- src/theme/Navbar/Logo/index.tsx
+++ src/theme/Navbar/Logo/index.tsx
@@ -4,7 +4,9 @@ import { WireLogoMark } from "@site/src/components/logos/wire/mark";
 
 export default function NavbarLogo(): JSX.Element {
   return (
-    <Link to="/" className="w-14 shrink text-darkest dark:text-white">
+    <Link
+      to="/"
+      className="w-14 shrink text-blue dark:text-white dark:hover:text-blue">
       <WireLogoMark className="w-44" />
     </Link>
   );
